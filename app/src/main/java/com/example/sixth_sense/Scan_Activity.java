f4f3c6662c76adc1dc8e544fa12b4f05a6b36a21
Julia Gimbernat <jg917@imperial.ac.uk>
2020-03-06 15:56:01 +0000

    hellooooo

--- app/src/main/java/com/example/sixth_sense/Scan_Activity.java
+++ app/src/main/java/com/example/sixth_sense/Scan_Activity.java
@@ -3,18 +3,29 @@ package com.example.sixth_sense;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.util.Log;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.Viewport;
 import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.DataPointInterface;
 import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.graphview.series.OnDataPointTapListener;
+import com.jjoe64.graphview.series.PointsGraphSeries;
+import com.jjoe64.graphview.series.Series;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
+
+
+import java.util.ArrayList;
+import java.util.Random;
+
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -41,6 +52,20 @@ public class Scan_Activity extends AppCompatActivity {
     String TimeStamp;
     String CSV_String = "";
 
+    // NEW CODE GRAPH
+    private static final String TAG = "MainActivity";
+
+    //add PointsGraphSeries of DataPoint type
+    PointsGraphSeries<DataPoint> xySeries;
+
+    PointsGraphSeries<DataPoint> onClickSeries;
+
+    //create graphview object
+    GraphView mScatterPlot;
+
+    //make xyValueArray global
+    ArrayList<XYValue> xyValueArray;
+    // NEW CODE GRAPH
 
 
     @Override
@@ -61,38 +86,71 @@ public class Scan_Activity extends AppCompatActivity {
         java.util.Date date = new Date();
         TimeStamp = formatter.format(date);
 
-        // we get graph view instance
-        GraphView graph = (GraphView) findViewById(R.id.graph);
-        // data
-        series = new LineGraphSeries<DataPoint>();
-        graph.addSeries(series);
-        // customize a little bit viewport
-        Viewport viewport = graph.getViewport();
-        viewport.setYAxisBoundsManual(true);
-        viewport.setMinY(0);
-        viewport.setMaxY(12);
-        viewport.setMinX(00);
-        viewport.setMaxX(50);
-        viewport.setScrollable(true);
-        viewport.setScalable(true);
-        graph.getGridLabelRenderer().setHorizontalAxisTitle("Potential (V)");
-        graph.getGridLabelRenderer().setVerticalAxisTitle("Current (A)");
-        if (android.os.Build.VERSION.SDK_INT > 9) {
-            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-            StrictMode.setThreadPolicy(policy);
+
+        // NEW CODE GRAPH
+
+        //declare graphview object
+        mScatterPlot = (GraphView) findViewById(R.id.graph);
+
+        xySeries = new PointsGraphSeries<>();
+
+        //generate two lists of random values, one for x and one for y.
+        xyValueArray = new ArrayList<>();
+        double start = -100;
+        double end = 100;
+        for(int i = 0; i<40; i++){
+            double randomX = new Random().nextDouble();
+            double randomY = new Random().nextDouble();
+            double x = start + (randomX * (end - start));
+            double y = start + (randomY * (end - start));
+            xyValueArray.add(new XYValue(x,y));
         }
-        // Saves CSV
-        try {
-            csv = getFilesDir() + "/" + TimeStamp + ".csv";
-            writer = new CSVWriter(new FileWriter(csv));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        //sort it in ASC order
+        xyValueArray = sortArray(xyValueArray);
+        //add the data to the series
+        for(int i = 0;i <xyValueArray.size(); i++){
+            double x = xyValueArray.get(i).getX();
+            double y = xyValueArray.get(i).getY();
+            xySeries.appendData(new DataPoint(x,y),true, 1000);
         }
 
+        createScatterPlot();
+
+        // NEW CODE GRAPH
+
+
+        // we get graph view instance
+        //GraphView graph = (GraphView) findViewById(R.id.graph);
+        // data
+        //series = new LineGraphSeries<DataPoint>();
+        //graph.addSeries(series);
+        // customize a little bit viewport
+        //Viewport viewport = graph.getViewport();
+        //viewport.setYAxisBoundsManual(true);
+        //viewport.setMinY(0);
+        //viewport.setMaxY(12);
+        //viewport.setMinX(00);
+        //viewport.setMaxX(50);
+        //viewport.setScrollable(true);
+        //viewport.setScalable(true);
+        //graph.getGridLabelRenderer().setHorizontalAxisTitle("Potential (V)");
+        //graph.getGridLabelRenderer().setVerticalAxisTitle("Current (A)");
+        //if (android.os.Build.VERSION.SDK_INT > 9) {
+            //StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            //StrictMode.setThreadPolicy(policy);
+        //}
+        // Saves CSV
+        //try {
+            //csv = getFilesDir() + "/" + TimeStamp + ".csv";
+            //writer = new CSVWriter(new FileWriter(csv));
+        //} catch (FileNotFoundException e) {
+            //e.printStackTrace();
+        //} catch (IOException e) {
+            //e.printStackTrace();
+        //}
+
     }
-    @Override
+    /*@Override
     protected void onResume() {
         super.onResume();
         // we're going to simulate real time with thread that append data to the graph
@@ -167,7 +225,134 @@ public class Scan_Activity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }*/
+
+
+    private void createScatterPlot() {
+        Log.d(TAG, "createScatterPlot: Creating scatter plot.");
+
+
+
+        xySeries.setOnDataPointTapListener(new OnDataPointTapListener() {
+            @Override
+            public void onTap(Series series, DataPointInterface dataPoint) {
+                Log.d(TAG, "onTap: You clicked on: (" + dataPoint.getX() +
+                        "," + dataPoint.getY() + ")");
+                //declare new series
+                onClickSeries = new PointsGraphSeries<>();
+
+                onClickSeries.appendData(new DataPoint(dataPoint.getX(),dataPoint.getY()),true, 100);
+
+                onClickSeries.setShape(PointsGraphSeries.Shape.RECTANGLE);
+
+                onClickSeries.setColor(Color.RED);
+
+                onClickSeries.setSize(25f);
+
+                mScatterPlot.removeAllSeries();
+
+                mScatterPlot.addSeries(onClickSeries);
+
+                toastMessage("x = " + dataPoint.getX() + "\n" +
+                        "y = " + dataPoint.getY() );
+
+                createScatterPlot();
+
+            }
+        });
+
+
+        //set some properties
+        xySeries.setShape(PointsGraphSeries.Shape.RECTANGLE);
+        xySeries.setColor(Color.BLUE);
+        xySeries.setSize(20f);
+
+        //set Scrollable and Scaleable
+        mScatterPlot.getViewport().setScalable(true);
+        mScatterPlot.getViewport().setScalableY(true);
+        mScatterPlot.getViewport().setScrollable(true);
+        mScatterPlot.getViewport().setScrollableY(true);
+
+        //set manual x bounds
+        mScatterPlot.getViewport().setYAxisBoundsManual(true);
+        mScatterPlot.getViewport().setMaxY(150);
+        mScatterPlot.getViewport().setMinY(-150);
+
+        //set manual y bounds
+        mScatterPlot.getViewport().setXAxisBoundsManual(true);
+        mScatterPlot.getViewport().setMaxX(150);
+        mScatterPlot.getViewport().setMinX(-150);
+
+        mScatterPlot.addSeries(xySeries);
     }
+
+    /**
+     * Sorts an ArrayList<XYValue> with respect to the x values.
+     * @param array
+     * @return
+     */
+    private ArrayList<XYValue> sortArray(ArrayList<XYValue> array){
+        /*
+        //Sorts the xyValues in Ascending order to prepare them for the PointsGraphSeries<DataSet>
+         */
+        int factor = Integer.parseInt(String.valueOf(Math.round(Math.pow(array.size(),2))));
+        int m = array.size()-1;
+        int count = 0;
+        Log.d(TAG, "sortArray: Sorting the XYArray.");
+
+        while(true){
+            m--;
+            if(m <= 0){
+                m = array.size() - 1;
+            }
+            Log.d(TAG, "sortArray: m = " + m);
+            try{
+                //print out the y entrys so we know what the order looks like
+                //Log.d(TAG, "sortArray: Order:");
+                //for(int n = 0;n < array.size();n++){
+                //Log.d(TAG, "sortArray: " + array.get(n).getY());
+                //}
+                double tempY = array.get(m-1).getY();
+                double tempX = array.get(m-1).getX();
+                if(tempX > array.get(m).getX() ){
+                    array.get(m-1).setY(array.get(m).getY());
+                    array.get(m).setY(tempY);
+                    array.get(m-1).setX(array.get(m).getX());
+                    array.get(m).setX(tempX);
+                }
+                else if(tempY == array.get(m).getY()){
+                    count++;
+                    Log.d(TAG, "sortArray: count = " + count);
+                }
+
+                else if(array.get(m).getX() > array.get(m-1).getX()){
+                    count++;
+                    Log.d(TAG, "sortArray: count = " + count);
+                }
+                //break when factorial is done
+                if(count == factor ){
+                    break;
+                }
+            }catch(ArrayIndexOutOfBoundsException e){
+                Log.e(TAG, "sortArray: ArrayIndexOutOfBoundsException. Need more than 1 data point to create Plot." +
+                        e.getMessage());
+                break;
+            }
+        }
+        return array;
+    }
+
+
+    /**
+     * customizable toast
+     * @param message
+     */
+    private void toastMessage(String message){
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
+    }
+
+
+
 }
 
 
